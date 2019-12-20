54108cd8a45feee705fe3b2f51f7a0e3077a7fe7
asanzma <alfredosanz.indra@gmail.com>
2019-12-20 11:26:51 +0100

    new set up for sprinboot. Index and controller. Close #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SFGPetClinicApplication02.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Alfredo Sanz
+ * @time dic. 2019
+ **/
+@SpringBootApplication
+public class SFGPetClinicApplication02 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SFGPetClinicApplication02.class, args);
+    }
+}
---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/IndexController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Alfredo Sanz
+ * @time dic. 2019
+ **/
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+}
